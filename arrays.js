067b513d18f600bc23fc2cacca00c12b3bfee80a
Laura Felce <crepu83@yahoo.it>
2018-09-12 09:37:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,12 +10,12 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray (array, element) {
-["element", ...array];
+[element, ...array];
 return array;
 } 
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-array.unshift ("element");
+array.unshift (element);
 return array;
 }
 
