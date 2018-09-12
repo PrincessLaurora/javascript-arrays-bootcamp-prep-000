2f91cbedd5be613ebaac2d670dba506b0ef93110
Laura Felce <crepu83@yahoo.it>
2018-09-12 09:49:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,4 +24,7 @@ function addElementToEndOfArray (array, element) {
   return array;
 }
 
-function addElementToEndOfArray (array, element) {
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push (element);
+  return array;
+}
